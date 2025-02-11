8432a0f0a91ac531d8195b9d47e57bf5c0090940
Syed Arslan <arslanhaider231@gmail.com>
2025-02-11 12:51:29 +0100

    complete search for countries lavel 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import CountriesLIst from "./CountriesList";
 import CountriesAll from "./CountriesAll.json";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
 function App() {
+  const [search, setSearch] = useState("");
+  const filteredCountries = CountriesAll.filter(
+    (country) =>
+      country.name.toLowerCase().includes(search.toLowerCase()) ||
+      country.capital.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <div className="container">
       <h1>REST Countries</h1>
-      <CountriesLIst countryName={CountriesAll} />
+      <SearchBar search={search} setSearch={setSearch} />
+      <CountriesLIst countryName={filteredCountries} />
     </div>
   );
 }
-
 export default App;
---
+++ src/SearchBar.tsx
@@ -0,0 +1,14 @@
+function SearchBar({ search, setSearch }) {
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search for a country or capital"
+        value={search}
+        className="search-bar"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </>
+  );
+}
+export default SearchBar;
