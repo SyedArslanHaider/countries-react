4f0dc9a719c1ebc61b20ea3f682b23583ee7207b
Syed Arslan <arslanhaider231@gmail.com>
2025-02-11 16:01:01 +0100

    level three complete select countries by region

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,30 @@ import CountriesLIst from "./CountriesList";
 import CountriesAll from "./CountriesAll.json";
 import SearchBar from "./SearchBar";
 import { useState } from "react";
+import SelectRegion from "./SelectRegion";
 function App() {
   const [search, setSearch] = useState("");
-  const filteredCountries = CountriesAll.filter(
-    (country) =>
-      country.name.toLowerCase().includes(search.toLowerCase()) ||
-      country.capital.toLowerCase().includes(search.toLowerCase())
-  );
+  const [region, setRegion] = useState("");
+
+  const filteredCountries = CountriesAll.filter((country) => {
+    // First, filter by region
+    if (region && country.region !== region) return false;
+
+    // Then, filter by name or capital
+    const matchesName = country.name
+      .toLowerCase()
+      .includes(search.toLowerCase());
+    const matchesCapital =
+      country.capital &&
+      country.capital.toLowerCase().includes(search.toLowerCase());
+
+    return matchesName || matchesCapital;
+  });
   return (
     <div className="container">
       <h1>REST Countries</h1>
       <SearchBar search={search} setSearch={setSearch} />
+      <SelectRegion region={region} setRegion={setRegion} />
       <CountriesLIst countryName={filteredCountries} />
     </div>
   );
---
+++ src/SelectRegion.tsx
@@ -0,0 +1,15 @@
+function SelectRegion({ region, setRegion }) {
+  return (
+    <>
+      <select value={region} onChange={(e) => setRegion(e.target.value)}>
+        <option value="">Filter by Region</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </>
+  );
+}
+export default SelectRegion;
